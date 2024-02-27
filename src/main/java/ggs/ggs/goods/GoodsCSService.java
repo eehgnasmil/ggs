c800b47b9dc7b593dd143b7cd75063fb9629eca8
jesuskisd <113132329+jesuskisd@users.noreply.github.com>
2024-02-27 18:00:32 +0900

    복구
    ㅋ

---
+++ src/main/java/ggs/ggs/goods/GoodsCSService.java
@@ -0,0 +1,17 @@
+package ggs.ggs.goods;
+
+import ggs.ggs.dto.GoodsQnADto;
+import ggs.ggs.dto.ReviewDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+
+public interface GoodsCSService {
+    void save(GoodsQnADto goodsQnADto);
+
+    Page<GoodsQnADto> findbyGoodsQnA(String id, int page);
+
+    ReviewDto findbyOrderItem(Integer idx);
+}
